12054716ecff6cb331fd091856cbe86746c1aab6
ilias bhallil <ilias.bhal@gmail.com>
2022-08-29 15:55:39 +0300

    add expect to test

--- src/tests/examples/fetch.test.ts
+++ src/tests/examples/fetch.test.ts
@@ -3,11 +3,12 @@ import { createNarration } from '../../entry';
 // Notes:
 // "while" is used to talk about states
 // "when" is used to describe events
+// "it starts" is used to create and or set initial state
 
 const makeFetch = () => {}
 
 it('example: fetch', () => {
-  const { it, while, when } = createNarration();
+  const { it, while, when, end, ctx } = createNarration();
 
   const PENDING = it.starts.as('PENDING');
   when.it.is(PENDING).it.starts(makeFetch);
@@ -21,6 +22,23 @@ it('example: fetch', () => {
   const HAS_DATA = it.can.be('HAS_DATA');
   const [,RECEIVE_DATA] = while(PENDING).when('RECEIVE_DATA').then.it.becomes(HAS_DATA)
   const [,SAVE_DATA] = when(RECEIVE_DATA).it.does('SAVE_DATA');
-  const [,REFETCH] = while(PENDING).when('REFETCH').then.it.becomes(PENDING);
+  const [,REFETCH] = while(HAS_DATA).when('REFETCH').then.it.becomes(PENDING);
   when(REFETCH).it.does(CLEAR_ERROR);
+
+  end();
+
+  expect(ctx.getConfig()).toMatchObject({
+    states: {
+      initial: 'PENDING',
+      PENDING: {
+        FETCH_FAILED: 'ERRORED',
+      },
+      ERRORED: {
+        RETRY: 'PENDING',
+      },
+      HAS_DATA: {
+        REFETCH: 'PENDING',
+      }
+    }
+  })
 });
